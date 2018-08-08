3a7371a823c428a7338db3c9910115750e226df5
isaacgr <isaacgrowell@gmail.com>
2018-08-08 07:28:01 -0400

    Install handlebars for templating Template home and about pages

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (request, response) => {
-  response.send({
-    name: "Isaac",
-    stuff: ["things"]
+  response.render("home.hbs", {
+    pageTitle: "Home Page",
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: "Hey"
   });
 });
 
 app.get("/about", (request, response) => {
-  response.send("About Page");
+  response.render("about.hbs", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/bad", (request, response) => {
