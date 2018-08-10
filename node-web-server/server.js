538efd7cf817efbdb0b8a23131a65b1e36d03f24
isaacgr <isaacgrowell@gmail.com>
2018-08-10 08:36:31 -0400

    Show use of handlebars templates Show use of handlebars partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,23 +1,46 @@
 const express = require("express");
 const hbs = require("hbs");
-
+const fs = require("fs");
 const app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
+
+app.use((request, response, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${request.method} ${request.url}`;
+
+  fs.appendFile("server.log", log + "\n", error => {
+    if (error) {
+      console.log(error);
+    }
+  });
+  next();
+});
+
+// app.use((request, response, next) => {
+//   response.render("maintenance.hbs");
+// });
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
 
 app.get("/", (request, response) => {
   response.render("home.hbs", {
     pageTitle: "Home Page",
-    currentYear: new Date().getFullYear(),
     welcomeMessage: "Hey"
   });
 });
 
 app.get("/about", (request, response) => {
   response.render("about.hbs", {
-    pageTitle: "About Page",
-    currentYear: new Date().getFullYear()
+    pageTitle: "About Page"
   });
 });
 
