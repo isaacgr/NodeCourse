d99e6fec40e52142c92078016cbf3bb885e8de7b
isaacgr <isaacgrowell@gmail.com>
2018-08-14 07:55:40 -0400

    Use assertion library to write test cases

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,26 @@
 const utils = require("./utils");
+const expect = require("expect");
 
 it("should add two numbers", () => {
   const result = utils.add(33, 11);
-  if (result !== 44) {
-    throw new Error(`Expected 44, got ${result} `);
-  }
+  expect(result)
+    .toBe(44)
+    .toBeA("number");
 });
 
 it("should square a number", () => {
   const result = utils.square(7);
-  if (result !== 49) {
-    throw new Error(`Expected 49, got ${result} `);
-  }
+  expect(result)
+    .toBe(49)
+    .toBeA("number");
+});
+
+it("should verify first and last names are set", () => {
+  const names = utils.setName({ age: 26 }, "Isaac Rowell");
+  expect(names)
+    .toInclude({
+      firstName: "Isaac",
+      lastName: "Rowell"
+    })
+    .toBeA("object");
 });
